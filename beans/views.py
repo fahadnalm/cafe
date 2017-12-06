db38d7edca581bbfacc5b1e854ca5afcff76f119
fahadnalm <almansouri.f@hotmail.com>
2017-12-06 21:03:35 +0300

    authinticate user

--- beans/views.py
+++ beans/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserSignup, UserLogin
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+
+def usersignup(request):
+	form = UserSignup()
+	context = {
+	'form': form
+	}
+	if request.method == 'POST':
+		form = UserSignup(request.POST)
+		if form.is_valid():
+			user = form.save()
+			username = user.username
+			password = user.password
+
+			user.set_password(password)
+			user.save()
+
+			auth_user = authenticate(username=username, password=password)
+			login(request, auth_user)
+
+			return redirect('')
+		return redirect('cafe:signup')
+	return render(request, 'signup.html', context)
+
+def userlogin(request):
+	form = UserLogin()
+	context = {
+	'form': form
+	}
+	if request.method == 'POST':
+		form = UserLogin(request.POST)
+		if form.is_valid():
+
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password']
+
+			auth_user = authenticate(username=username, password=password)
+			if auth_user is not None:
+				login(request, auth_user)
+				return redirect('')
+			return redirect('cafe:login')
+		return redirect('cafe:login')
+	return render(request, 'login.html', context)
+
+def userlogout(request):
+	logout(request)
+	return redirect('')
+	
+
