5765bb91ca2e6dbbb23a3317b7267ab37c38ec0e
fahadnalm <almansouri.f@hotmail.com>
2017-12-09 16:28:32 +0300

    list & update

--- beans/urls.py
+++ beans/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
 	path('login/', views.userlogin, name='login'),
 	path('logout/', views.userlogout, name='logout'),
 	path('create/', views.create_coffee, name='create'),
+	path('ajax_price/', views.ajax_price, name="ajax_price"),
+	path('coffee_list/', views.coffee_list, name='coffee_list'),
+	path('coffee_detail/<int:coffee_detail>', views.coffee_detail, name="coffee_detail"),
 
 ]
--- beans/views.py
+++ beans/views.py
@@ -2,6 +2,42 @@ from django.shortcuts import render, redirect
 from .forms import UserSignupForm, UserLoginForm, CoffeeForm
 from django.contrib.auth import authenticate, login, logout
 from decimal import Decimal
+from django.http import JsonResponse, Http404
+from .models import Bean, Roast, Syrup, Powder, Coffee
+import json
+
+
+
+def ajax_price(request):
+    total_price = Decimal(0)
+
+    bean_id = request.GET.get('bean')
+    if bean_id:
+        total_price += Bean.objects.get(id=bean_id).price
+
+    roast_id = request.GET.get('roast')
+    if roast_id:
+        total_price += Roast.objects.get(id=roast_id).price
+
+    syrups = json.loads(request.GET.get('syrups'))
+    if len(syrups)>0:
+        for syrup_id in syrups:
+            total_price += Syrup.objects.get(id=syrup_id).price
+
+    powders = json.loads(request.GET.get('powders'))
+    if len(powders)>0:
+        for powder_id in powders:
+            total_price += Powder.objects.get(id=powder_id).price
+
+    milk = request.GET.get('milk')
+    if milk=='true':
+        total_price += Decimal(0.100)
+
+    shots=request.GET.get('espresso_shots')
+    if shots:
+        total_price += (int(shots)*Decimal(0.250))
+
+    return JsonResponse(round(total_price,3), safe=False)
 
 
 
@@ -78,7 +114,22 @@ def create_coffee(request):
             form.save_m2m()
             coffee.price = coffee_price(coffee)
             coffee.save()
-            return redirect('/')
+            return redirect('')
     context['form'] = form
     return render(request, 'create_coffee.html', context)
+
+def coffee_list(request):
+    if not request.user.is_authenticated():
+        return redirect("cafe:login")
+
+    coffee_list = Coffee.objects.filter(user=request.user)
+    return render(request, 'coffee_list.html', {'coffee_list': coffee_list})
+
+def coffee_detail(request, coffee_id):
+    if not request.user.is_authenticated():
+        return redirect("cafe:login")
+    coffee = Coffee.objects.get(id=coffee_id)
+    if not (request.user == coffee.user or request.user.is_superuser or request.user.is_staff):
+        raise Http404
+    return render(request, 'coffee_detail.html', {'coffee': coffee})
 
